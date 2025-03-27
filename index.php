4d4f97e1e227e5544439a8c63bd3af0ed42b7557
Hridoy <154232641+1dev-hridoy@users.noreply.github.com>
2025-03-27 22:48:49 +0600

    Add navbar and hero section components to index.php

--- index.php
+++ index.php
@@ -1,63 +1,9 @@
 <?php
 include_once './includes/components/header.php';
+include_once './includes/components/navbar.php';
+include_once './includes/sections/__hero__section.php';
 ?>
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand" href="#">PublicQuote</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#how-it-works">How It Works</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#api">API</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#testimonials">Testimonials</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#faq">FAQ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
 
-<!-- Hero Section -->
-<section class="hero">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-6" data-aos="fade-right" data-aos-duration="1000">
-                <h1 class="hero-title">Wisdom in <span>Words</span>, <br>Just a Click Away</h1>
-                <p class="hero-subtitle">Discover inspiring quotes from history's greatest minds. Perfect for your apps, websites, or daily inspiration.</p>
-                <div class="d-flex gap-3">
-                    <button class="btn btn-primary btn-lg" id="generate-btn">Generate a Quote</button>
-                    <button class="btn btn-outline-primary btn-lg">Learn More</button>
-                </div>
-            </div>
-            <div class="col-lg-6 d-none d-lg-block" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
-                <div class="hero-image">
-                    <img src="assets/images/hero.jpg" alt="Quotes Illustration" class="img-fluid">
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="hero-shape">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#ffffff" fill-opacity="1" d="M0,160L48,149.3C96,139,192,117,288,122.7C384,128,480,160,576,165.3C672,171,768,149,864,149.3C960,149,1056,171,1152,165.3C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-        </svg>
-    </div>
-</section>
 
 <!-- Random Quotes Section -->
 <section class="section section-light" id="quotes">
